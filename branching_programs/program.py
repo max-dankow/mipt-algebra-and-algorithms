513e80060e3b57c85edf11a6875c5969a33bc5bf
Данковцев Максим <max.dankow@gmail.com>
2018-12-10 05:00:54 +0300

    task 10' accepted

--- branching_programs/program.py
+++ branching_programs/program.py
@@ -9,8 +9,8 @@ NOT = -1
 OR = 1
 AND = 2
 
-WELL_KNOWN_CYCLE_A = np.array([2, 3, 4, 5, 1]) - 1
-WELL_KNOWN_CYCLE_B = np.array([3, 5, 2, 1, 4]) - 1
+WELL_KNOWN_CYCLE_A = np.array([1, 2, 3, 4, 0])
+WELL_KNOWN_CYCLE_B = np.array([2, 4, 1, 0, 3])
 IDENTITY_CYCLE = np.arange(5)
 
 
@@ -42,10 +42,12 @@ def replace_disjunction(circuit):
 
     return circuit
 
+
 # pbp := ((cycle), [instruction])
 # instruction := (variable id, cycle on true, cycle on false)
 # k-cycle = np.array() of length k
 # permutation - 2 arrays
+
 
 def apply_cycle(cycle, values):
     current = values[cycle[-1]]
@@ -72,11 +74,12 @@ def compose(*argv):
     result = argv[0]
     for perm in argv[1:]:
         result = np.arange(len(result))[perm[result]]
-        # todo: there could be an error with order of composition
     return result
+
 
 def revert(perm):
     return np.argsort(perm)
+
 
 def cycle_to_chain(cycle):
     k = 0
@@ -85,79 +88,74 @@ def cycle_to_chain(cycle):
         result.append(cycle[k])
         k = cycle[k]
     assert k == 0, 'Cycle is not actually cyclic'
-    # print('Cycle', cycle,'to chain', result)
     return np.array(result)
 
+
 def change_permutation(pbp, new_cycle):
-    # print('original before transform', pbp)
-    # print('new cycle', new_cycle)
-    gamma = cycle_to_chain(new_cycle)[np.argsort(cycle_to_chain(pbp[0]))]
+    assert_cycle(new_cycle)
+    gamma = cycle_to_chain(pbp[0])[np.argsort(cycle_to_chain(new_cycle))]
     gamma_rev = revert(gamma)
-    # print('gamma', gamma)
-    # print('gamma_rev', gamma_rev)
 
     new_commands = pbp[1].copy()
     new_commands[0] = (new_commands[0][0],
-        compose(gamma, new_commands[0][1]),\
-        compose(gamma, new_commands[0][2]))
+                       compose(gamma, new_commands[0][1]),
+                       compose(gamma, new_commands[0][2]))
 
     new_commands[-1] = (new_commands[-1][0],
-        compose(new_commands[-1][1], gamma_rev),\
-        compose(new_commands[-1][2], gamma_rev))
+                        compose(new_commands[-1][1], gamma_rev),
+                        compose(new_commands[-1][2], gamma_rev))
 
     new_pbp = (new_cycle, new_commands)
 
-    # print('original after transform', pbp)
-    # print('new after transform', new_pbp)
     return new_pbp
 
+
 def build_var(var):
-    # print(var)
     new_pbp = (WELL_KNOWN_CYCLE_A, [(var, WELL_KNOWN_CYCLE_A, IDENTITY_CYCLE)])
-    # print(new_pbp)
     return new_pbp
 
 
 def build_not(pbp):
-    # print('original before not', pbp)
     sigma = pbp[0]
     sigma_rev = revert(sigma)
     new_pbp = change_permutation(pbp, sigma_rev)
-    new_pbp[1][-1] = (new_pbp[1][-1][0], \
-        compose(pbp[1][-1][1], sigma_rev),\
-        compose(pbp[1][-1][2], sigma_rev))
-    # print('original after not', pbp)
-    # print('new after not', new_pbp)
+    new_pbp[1][-1] = (new_pbp[1][-1][0],
+                      compose(pbp[1][-1][1], sigma_rev),
+                      compose(pbp[1][-1][2], sigma_rev))
     return new_pbp
 
 
 def build_and(pbp1, pbp2):
-    # print(pbp1)
-    # print(pbp2)
     sigma = WELL_KNOWN_CYCLE_A
     tau = WELL_KNOWN_CYCLE_B
     pbp1_new = change_permutation(pbp1, sigma)
     pbp2_new = change_permutation(pbp2, tau)
 
-    # print(pbp1_new)
-    # print(pbp2_new)
-
     sigma_rev = revert(sigma)
     tau_rev = revert(tau)
-    pbp1_rev = change_permutation(pbp1_new, sigma_rev)
-    pbp2_rev = change_permutation(pbp2_new, tau_rev)
+    pbp1_rev = change_permutation(pbp1, sigma_rev)
+    pbp2_rev = change_permutation(pbp2, tau_rev)
+
+    assert_identity(sigma, sigma_rev)
+    assert_identity(tau, tau_rev)
+
+    assert_identity(sigma, IDENTITY_CYCLE, sigma_rev, IDENTITY_CYCLE)
+    assert_identity(tau, IDENTITY_CYCLE, tau_rev, IDENTITY_CYCLE)
 
     new_perm = compose(sigma, tau, sigma_rev, tau_rev)
+
+    assert_not_identity(sigma, tau)
+    assert_not_identity(tau, sigma_rev)
+    assert_not_identity(sigma_rev, tau_rev)
+    assert_not_identity(tau_rev, sigma)
+    assert_not_identity(new_perm)
     new_commands = pbp1_new[1] + pbp2_new[1] + pbp1_rev[1] + pbp2_rev[1]
-    # print(new_perm)
-    # print(new_commands)
 
     return (new_perm, new_commands)
 
 
 def build_rec(circuit, current, pbps):
     # Caller should check if result is already calculated
-    # print(current)
     type_ = circuit[current][0]
     if type_ == VAR:
         pbps[current] = build_var(current)
@@ -177,23 +175,42 @@ def build_rec(circuit, current, pbps):
     else:
         assert False, 'Circuit contains OR, but it shouldn''t'
 
+
 def buid_pbp(circuit, output_node):
     current = 0
     pbps = np.empty(shape=(len(circuit),), dtype=tuple)
-    # print(pbps)
 
     for _ in range(len(circuit)):
         if pbps[current] is None:
             build_rec(circuit, current, pbps)
         current += 1
-        # print(pbps)
-
 
     return pbps[output_node]
 
-# Tests
+
+# Test
 
 
+def assert_not_identity(*argv):
+    assert not np.all(compose(*argv) - IDENTITY_CYCLE == 0)
+
+
+def assert_identity(*argv):
+    assert np.all(compose(*argv) - IDENTITY_CYCLE == 0)
+
+
+def assert_cycle(perm):
+    visited = np.zeros_like(perm)
+    count = 1
+    k = 0
+    visited[k] = 1
+    while True:
+        k = perm[k]
+        if visited[k] == 1:
+            assert count == len(perm)
+            return
+        visited[k] = 1
+        count += 1
 
 
 # Application
@@ -229,21 +246,26 @@ def print_result(pbp, circuit):
         alpha = cmd[1]
         beta = cmd[2]
         for t in range(len(cmd[1])):
-            print(var_name, k + alpha[t], k + beta[t])
+            print(var_name, k + beta[t], k + alpha[t])
         k += 5
     output = np.array([False, True, True, True, True])
     for x in output:
-        print(x)
+        if x:
+            print('TRUE')
+        else:
+            print('FALSE')
+
 
 def run_solution():
     circuit = get_input()
     output_node = len(circuit) - 1
-    # print(circuit)
     circuit = replace_disjunction(circuit)
-    # print(circuit)
     result = buid_pbp(circuit, output_node)
-    # print('RESULT', result)
     print_result(result, circuit)
 
+
+assert_cycle(WELL_KNOWN_CYCLE_A)
+assert_cycle(WELL_KNOWN_CYCLE_B)
+assert_identity(IDENTITY_CYCLE)
 
 run_solution()
